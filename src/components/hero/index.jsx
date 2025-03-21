1b3a99091f1bab4a1f3b2c094512527b1d67939b
Pedro_Barboza <phbarbozafarias@gmail.com>
2025-03-21 14:57:55 -0300

    fix: fixing the hero image

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,6 +1,6 @@
 function Hero() {
     return (
-        <section className="bg-[url(./images/default-1.jpg)] bg-no-repeat bg-center bg-cover w-screen h-[19.5rem] lg:h-[22rem] overflow-clip">
+        <section className="bg-[url(/images/default-1.jpg)] bg-no-repeat bg-center bg-cover w-screen h-[19.5rem] lg:h-[22rem] overflow-clip">
             <div className="grid p-6 pt-24 bg-gradient-to-b from-gray-700/85 to-gray-900/50 h-full lg:place-items-center lg:text-center">
                 <p className="w-[20rem] md:w-[28rem] text-3xl md:text-3xl lg:text-5xl ">
                     Encontre <span className="text-green-500">os melhores livros</span> para os seus estudo <span className="text-green-500">aqui</span>!
