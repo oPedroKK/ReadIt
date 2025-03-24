65bde9d25a8c0305710b12e364bfe655ff04fed9
Pedro_Barboza <phbarbozafarias@gmail.com>
2025-03-24 08:59:12 -0300

    style: changing footers infomations

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <section className="grid py-4 w-screen text-black bg-[#e8ecef] place-items-center">
             <div className="text-center py-4">
-                <p>Copyright © 2025 <spoiler className="font-bold">Pedro Barboza</spoiler></p>
+                <p>Copyright © 2025<spoiler className="font-bold"> ReadIt </spoiler>by<spoiler className="font-bold"> Pedro Barboza</spoiler>.</p>
                 <a href="#" className="underline">Política de Privacidade</a>
             </div>
         </section>
